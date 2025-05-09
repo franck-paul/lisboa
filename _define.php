4a4674a1d1d743af0b9f73f80009ad518b823eac
franck <carnet.franck.paul@gmail.com>
2025-05-09 15:38:59 +0200

    New release 1.13

--- _define.php
+++ _define.php
@@ -13,9 +13,9 @@ $this->registerModule(
     'Lisboa',
     'Dotclear 2.34 theme',
     'Dotclear Team',
-    '1.12',
+    '1.13',
     [
-        'date'     => '2025-05-09T10:26:21+0200',
+        'date'     => '2025-05-09T15:38:59+0200',
         'requires' => [['core', '2.34']],
         'type'     => 'theme',
         'tplset'   => 'dotty',
